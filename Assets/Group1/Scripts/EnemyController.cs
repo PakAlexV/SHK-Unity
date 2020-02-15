8cc765a3f7ff6ea87c5b321e548175467c21f457
Alex <pak.alex.v@gmail.com>
2020-02-15 15:22:39 +0700

    refactoring game

---
+++ Assets/Group1/Scripts/EnemyController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] private Transform _selfTransform;
+    [SerializeField] private int _speed = 2;
+    private GameController _controller; 
+    private Vector3 _nextPosition;
+
+    private void Start()
+    {
+        _controller = GameObject.FindObjectOfType<GameController>();
+        NextPositionInit();
+    }
+
+    private void NextPositionInit()
+    {
+        _nextPosition = Random.insideUnitCircle * _controller.RangePositionEnemy;
+    }
+
+    private void MoveEnemy()
+    {
+        _selfTransform.position = Vector3.MoveTowards(_selfTransform.position, _nextPosition, _speed * Time.deltaTime);
+        if (_selfTransform.position == _nextPosition) { NextPositionInit(); }      
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerController>() != null)
+        {
+            Vector3 playerPosition = collision.GetComponent<Transform>().position;
+            Vector3 enemyPosition = gameObject.GetComponent<Transform>().position;
+            if (Vector3.Distance(playerPosition, enemyPosition) < 0.2f) { _controller.EnemyKill(gameObject); }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        MoveEnemy();      
+    }
+}
---
+++ Assets/Group1/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Transform _selfTransform;
+    [SerializeField] private int _speedPlayer = 3;
+    
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W)) { _selfTransform.Translate(0, _speedPlayer * Time.deltaTime, 0); }       
+        if (Input.GetKey(KeyCode.S)) { _selfTransform.Translate(0, -_speedPlayer * Time.deltaTime, 0); }
+        if (Input.GetKey(KeyCode.A)) { _selfTransform.Translate(-_speedPlayer * Time.deltaTime, 0, 0); }
+        if (Input.GetKey(KeyCode.D)) { _selfTransform.Translate(_speedPlayer * Time.deltaTime, 0, 0); }        
+    }
+}
